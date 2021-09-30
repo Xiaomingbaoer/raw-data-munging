a63617968eb381b4846a3dcc331dd725826007d1
Yuanpei Wang <yw4392@nyu.edu>
2021-09-30 04:54:51 -0400

    Clear data and analyze average decade changes

--- analyze.py
+++ analyze.py
@@ -1,1 +1,36 @@
 # Place code below to do the analysis part of the assignment.
+import csv
+
+def ave(a):
+    result=0
+    for i in a:
+        result+=float(i)
+    return format(result/10, ".2f")
+
+csvfile=open('clean_data.csv',newline='')
+tempreader=csv.reader(csvfile,delimiter=',',quotechar='|')
+JD=[]
+lastyear=0
+for row in tempreader:
+    #print(row)
+    JD.append(row[13])
+    if row[0]=="2021":
+        #print(row)
+        for i in row[1:9]:
+           lastyear+=float(i)
+
+newJD=JD[1:-2]
+lasttwoline=JD[-2:]
+#print(lasttwoline)
+allmean=[]
+for i in range(0,140,10):
+    lst=[]
+    a=0
+    while a<10:
+        lst.append(newJD[i+a])
+        a+=1
+    allmean.append(ave(lst))
+
+lasttwoyears=(lastyear+float(lasttwoline[0])*12)/20
+allmean.append(format(lasttwoyears,".2f"))
+print(allmean)
--- munge.py
+++ munge.py
@@ -1,1 +1,31 @@
 # Place code below to do the munging part of this assignment.
+rawdata=open('data.txt','r').readlines()[8:]
+heading=open('data.txt','r').readlines()[7]
+heading=','.join(heading.split()[:-1])+'\n'
+cleandata=[]
+cleandata.append(heading)
+#print(heading)
+for line in rawdata:
+    if line[0].isnumeric():
+        newline=line.split()
+        year=newline[0:1]  #year column 
+        newline_no_year=newline[1:]#the others
+        for i in newline_no_year:
+            if i=='***':
+               newline_no_year[newline_no_year.index('***')]='NaN'
+            elif i=='****':
+                newline_no_year[newline_no_year.index('****')]='NaN'
+            else:
+                a=newline_no_year.index(i)
+                newline_no_year[a]=format(float(newline_no_year[a])*0.018,".1f")#C -> F
+        year+=newline_no_year
+        year_new=year[:-1]
+        year_new.append('\n')
+        newyear=','.join(year_new)
+        cleandata.append(newyear)
+newcleandata=''.join(cleandata)
+
+#save the data to csv file
+f=open('clean_data.csv','w')
+f.write(newcleandata)
+f.close()
